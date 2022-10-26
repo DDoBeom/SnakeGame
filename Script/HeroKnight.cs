76c7ed30e0b0d69b192c412e0a5e2d2f4c3523df
DDoBeom <116055314+DDoBeom@users.noreply.github.com>
2022-10-27 00:25:43 +0900

    Add files via upload

---
+++ Script/HeroKnight.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroKnight : HeroComponent
+{
+    public override void Attack(Vector3 pos)
+    {
+        isAttack = true;
+        GameObject weap = Instantiate(weapon, transform.position, Quaternion.identity);
+        weap.transform.SetParent(transform.parent);
+        weap.transform.rotation = transform.rotation;
+
+        StartCoroutine(AttackTimer());
+    }
+}
